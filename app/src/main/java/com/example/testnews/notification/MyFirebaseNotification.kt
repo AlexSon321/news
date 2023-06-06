9cd716a5d163829eeb5dcfb204a4784e37bbf97c
admin <smartinyk33@gmail.com>
2023-06-06 06:20:14 -0500

    new

--- app/src/main/java/com/example/testnews/notification/MyFirebaseNotification.kt
+++ app/src/main/java/com/example/testnews/notification/MyFirebaseNotification.kt
@@ -1,7 +1,6 @@
 package com.example.testnews.notification
 
 import android.annotation.SuppressLint
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -10,6 +9,8 @@ import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
+import com.example.testnews.MainActivity
+import com.example.testnews.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -17,11 +18,12 @@ import com.google.firebase.messaging.RemoteMessage
 const val channelId = "notification_channel"
 const val channelName = "com.example.testnews"
 
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class MyFirebaseNotification: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
 
-      if(message.getNotification() != null){
+      if(message.notification != null){
           genereateNot(message.notification!!.title!!,message.notification!!.body!!)
       }
 
@@ -37,6 +39,7 @@ class MyFirebaseNotification: FirebaseMessagingService() {
         return remoteView
     }
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     fun genereateNot(title: String, author: String){
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
